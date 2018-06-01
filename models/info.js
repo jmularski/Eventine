083c163b3abfc1c456a52e127d629688a90b8d2c
Jakub Mularski <dragt12@o2.pl>
2018-06-02 01:24:53 +0200

    Small fixes in info

--- models/info.js
+++ models/info.js
@@ -5,7 +5,7 @@ var InfoSchema = new Schema({
     groupId: { type: String, required: true },
     creator: { type: String, required: true },
     content: { type: String, required: true },
-    targetGroups: { type: String, required: true }
+    targetGroups: { type: [String], required: true }
 });
 
-module.exports = mongoose.model('info', GroupSchema);
+module.exports = mongoose.model('info', InfoSchema);
