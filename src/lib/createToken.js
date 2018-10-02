07841b48fdf71797090d15c3ace53d6494efcb94
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-02 20:15:21 +0200

    Create token fix

--- src/lib/createToken.js
+++ src/lib/createToken.js
@@ -1,4 +1,4 @@
 const jwt = require('jsonwebtoken');
-module.exports = () => {
+module.exports = (fullName, id) => {
     jwt.sign({'fullName': fullName, 'id': id}, 'kalejdoskop', {expiresIn: '7d'});
 }
