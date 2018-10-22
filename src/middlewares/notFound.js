7fbb552846285569ec04b938a49b843103836587
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-22 23:14:46 +0200

    Get acme-challenge running

--- src/middlewares/notFound.js
+++ src/middlewares/notFound.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
-    let err = new Error('Page is missing!');
-    err.status = 404;
-    next(err);
+    //let err = new Error('Page is missing!');
+    //err.status = 404;
+    //next(err);
+    res.send("This is a test page!")
 };
