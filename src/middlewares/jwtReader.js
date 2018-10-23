5581defa8ff4edb9145a62871f050722ab1dbca4
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-23 12:34:55 +0200

    IDK

--- src/middlewares/jwtReader.js
+++ src/middlewares/jwtReader.js
@@ -4,5 +4,6 @@ module.exports = (req, res, next) => {
     const token = req.headers['x-token'];
     let result = decryptToken(token);
     if (!result.success) res.status(401).send(result);
+    console.log(result)
     req.token = result.token;
 };
