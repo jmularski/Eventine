8a8332adfbc87de8844aeed240057c67112feb43
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-24 00:19:36 +0200

    Added logs

--- src/lib/sendNotif.js
+++ src/lib/sendNotif.js
@@ -4,7 +4,7 @@ require('winston-loggly-bulk');
 module.exports = async (payload, notifIds) => {
     try {
         if(notifIds.length > 0) await admin.messaging().sendToDevice(notifIds, payload);
-        
+        winston.log('log', 'Notification sent', {tags: 'notif'});
     } catch(e){
         winston.log('error', "Error with sending notification!", {tags: "notif"});
     }
