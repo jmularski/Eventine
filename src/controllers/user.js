df27063a6feafbce7f59d71723c63d9d24ef4133
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-24 05:09:26 +0200

    Mini commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -119,8 +119,8 @@ let setCaretaker = async (req, res) => {
 
 let callCaretaker = async (req, res) => {
     let { id } = req.token;
-    let caretakerId = await User.find({id}).careTaker;
-    let caretakerNotifToken = await User.find({caretakerId}).notifToken;
+    let caretakerId = await User.findOne({id}).careTaker;
+    let caretakerNotifToken = await User.findOne({caretakerId}).notifToken;
     let payload = {
         notification: {
             title: "Zawołanie opiekuna",
