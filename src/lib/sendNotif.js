bb8a46529bcb78925cf4508a657ea353d7c1094f
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-24 06:08:47 +0200

    Fix?

--- src/lib/sendNotif.js
+++ src/lib/sendNotif.js
@@ -3,7 +3,7 @@ var winston = require('winston');
 require('winston-loggly-bulk');
 module.exports = async (payload, notifIds) => {
     try {
-        notifIds = notifIds.filter((el) => { return el != null });
+        if(Array.isArray(notifIds)) notifIds = notifIds.filter((el) => { return el != null });
         if(notifIds.length > 0) await admin.messaging().sendToDevice(notifIds, payload);
         winston.log('log', 'Notification sent', {tags: 'notif'});
     } catch(e){
